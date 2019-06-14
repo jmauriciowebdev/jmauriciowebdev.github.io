2374c93dc5993e74c45e1123b8ebc4c5f1d3cf02
jpmwebdev <47918192+jpmwebdev@users.noreply.github.com>
2019-06-14 12:12:55 +0100

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,7 @@
 				}, 100);
 			});
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+
 
 		// Banner.
 			var $banner = $('#banner');
@@ -64,27 +63,6 @@
 		// Scrolly.
 			$('.scrolly').scrolly();
 
-		// Poptrox.
-			$window.on('load', function() {
-
-				var $thumbs = $('.thumbnails');
-
-				if ($thumbs.length > 0)
-					$thumbs.poptrox({
-						onPopupClose: function() { $body.removeClass('is-covered'); },
-						onPopupOpen: function() { $body.addClass('is-covered'); },
-						baseZIndex: 10001,
-						useBodyOverflow: false,
-						overlayColor: '#222226',
-						overlayOpacity: 0.75,
-						popupLoaderText: '',
-						fadeSpeed: 500,
-						usePopupDefaultStyling: false,
-						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-					});
-
-			});
-
 		// Initial scroll.
 			$window.on('load', function() {
 				$window.trigger('scroll');
@@ -93,3 +71,15 @@
 	});
 
 })(jQuery);
+
+
+
+function initMap() {
+    // Barreiro's Location
+    var barreiro = {lat: 38.661330, lng: -9.079156};
+    // Center map at Barreiro , zoom '10' allows to see Barreiro and a bit of Lisbon
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 10, center: barreiro});
+    // Place Marker at Barreiro
+    var marker = new google.maps.Marker({position: barreiro, map: map});
+  };
