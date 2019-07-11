baf5ce295fc94e7afcf23c1b8bf578622bda4141
jpmwebdev <47918192+jpmwebdev@users.noreply.github.com>
2019-07-11 12:56:23 +0100

    React Tests

---
+++ recipes/precache-manifest.dd9be36156b94b2f827eaf1db60be510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216cd8a961bbbe2a69ba0dbc8e49e37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3b0b3a70fa9aab7c61e",
+    "url": "/static/css/main.5dd7ae59.chunk.css"
+  },
+  {
+    "revision": "ff70012f966638d08fc3",
+    "url": "/static/js/2.e58cc85b.chunk.js"
+  },
+  {
+    "revision": "d3b0b3a70fa9aab7c61e",
+    "url": "/static/js/main.3f9f7918.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
